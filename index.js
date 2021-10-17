88b725b379fa845a64107f2a790f4ed4dcd7320a
Uzzal Ahmed <uzzalhamed0001@gmail.com>
2021-10-17 22:42:31 +0600

    Button customized

---
+++ index.js
@@ -0,0 +1,25 @@
+// function changeValue(a) {
+//     console.log('Value of a inside function before changing: ' + a);
+//     a = 10;
+//     console.log('Value of a inside function after changing: ' + a);
+//     return a;
+// }
+
+// var a = 100;
+// console.log('Value of a before changing function is applied: ' + a);
+// changeValue(a);
+// console.log('Value of a after changing function is applied: ' + a);
+
+// function demo(a) {
+//     console.log("Inside first a",a);
+//     b= 30;
+//     console.log("Inside second b= ", b);
+//     return a;
+// }
+// console.log("First a=", a);
+// var a = 100;
+// demo(a);
+// a=40;
+// console.log("Last a=", a);
+a=33;
+console.log(a);
